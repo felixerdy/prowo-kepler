2d09614b5dd299d737e500bfedc589051f560b99
PaulReichmuth <p.n.reichmuth@gmail.com>
2021-09-09 19:28:37 +0200

    Fixed missing modal labels

--- src/app.js
+++ src/app.js
@@ -429,9 +429,7 @@ class App extends Component {
               fontSize: "16px",
             }}
           >
-            This Kepler.gl component will always load a fresh state when re
-            mounted, state inside this component will be destroyed once its
-            unmounted.
+            Die Luftfeuchte
             <Closebutton buttonColor="transparent" onClick={this._closeModal}>
               ❌
             </Closebutton>
@@ -560,7 +558,7 @@ class App extends Component {
               fontSize: "16px",
             }}
           >
-            Luftdruck
+           Die Feinstaubbelastung, gemessen in PM2.5.
             <Closebutton buttonColor="transparent" onClick={this._closeModal}>
               ❌
             </Closebutton>
